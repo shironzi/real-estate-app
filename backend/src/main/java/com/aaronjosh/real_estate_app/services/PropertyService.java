df21e25fae4ef45f884dd8d6392f8d6701c87723
shironzi <157195223+shironzi@users.noreply.github.com>
2025-09-23 22:29:03 +0800

    feat: add FileEntity and FileRepository for image handling in property management; update PropertyDto and PropertyService to support image uploads

--- backend/src/main/java/com/aaronjosh/real_estate_app/services/PropertyService.java
+++ backend/src/main/java/com/aaronjosh/real_estate_app/services/PropertyService.java
@@ -6,10 +6,12 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.http.HttpStatus;
 
 import com.aaronjosh.real_estate_app.dto.PropertyDto;
+import com.aaronjosh.real_estate_app.models.FileEntity;
 import com.aaronjosh.real_estate_app.models.PropertyEntity;
 import com.aaronjosh.real_estate_app.models.UserEntity.Role;
 import com.aaronjosh.real_estate_app.repositories.PropertyRepository;
@@ -58,6 +60,21 @@ public class PropertyService {
         property.setPrice(propertyDto.getPrice());
         property.setTotalBedroom(propertyDto.getTotalBedroom());
         property.setPropertyType(propertyDto.getPropertyType());
+
+        for (MultipartFile image : propertyDto.getImages()) {
+            try {
+                FileEntity file = new FileEntity();
+
+                file.setName(image.getOriginalFilename());
+                file.setType(image.getContentType());
+                file.setData(image.getBytes());
+
+                file.setPropertyEntity(property);
+                property.getImages().add(file);
+            } catch (java.io.IOException e) {
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to process image file", e);
+            }
+        }
 
         return propertyRepo.save(property);
     }
