bd0c9e6d627fc325c62a664f21e331dc66899cb5
notsoeyvah <avarce15@gmail.com>
2025-10-21 11:15:23 +0800

    feat: update registration form and authentication utility functions

--- frontend/src/pages/auth/Register.tsx
+++ frontend/src/pages/auth/Register.tsx
@@ -5,179 +5,182 @@ import { register } from "@/utils/auth";
 import { Link, useNavigate } from "react-router-dom";
 
 const Register = () => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
-  const [firstname, setFirstname] = useState("");
-  const [lastname, setLastname] = useState("");
-  const [message, setMessage] = useState("");
-  const [isError, setIsError] = useState(true);
-  const [countdown, setCountdown] = useState(0);
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [showPassword, setShowPassword] = useState(false);
+	const [confirmPassword, setConfirmPassword] = useState("");
+	const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+	const [firstname, setFirstname] = useState("");
+	const [lastname, setLastname] = useState("");
+	const [message, setMessage] = useState("");
+	const [isError, setIsError] = useState(true);
+	const [countdown, setCountdown] = useState(0);
 
-  const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
-  };
+	const togglePasswordVisibility = () => {
+		setShowPassword(!showPassword);
+	};
 
-  const toggleConfirmPasswordVisibility = () => {
-    setShowConfirmPassword(!showConfirmPassword);
-  };
+	const toggleConfirmPasswordVisibility = () => {
+		setShowConfirmPassword(!showConfirmPassword);
+	};
 
-  const onSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    setMessage("");
-    setCountdown(0);
+	const onSubmit = async (e: React.FormEvent) => {
+		e.preventDefault();
+		setMessage("");
+		setCountdown(0);
 
-    if (password !== confirmPassword) {
-      setMessage("Password does not match.");
-      return;
-    }
+		console.log(password);
 
-    try {
-      const res = await register(
-        firstname,
-        lastname,
-        email,
-        password,
-        confirmPassword
-      );
+		if (password !== confirmPassword) {
+			setMessage("Password does not match.");
+			return;
+		}
 
-      setIsError(false);
-      setMessage(res?.data?.message ?? "Registration successful");
+		try {
+			const res = await register(
+				firstname,
+				lastname,
+				email,
+				password,
+				confirmPassword
+			);
 
-      // Start countdown (5 seconds)
-      setCountdown(5);
-    } catch (err: any) {
-      setMessage(err.message);
-      setIsError(true);
-    }
-  };
+			setIsError(false);
+			setMessage(res?.data?.message ?? "Registration successful");
 
-  // Countdown effect
-  useEffect(() => {
-    if (countdown > 0) {
-      const timer = setTimeout(() => {
-        setCountdown((prev) => prev - 1);
-      }, 1000);
+			// Start countdown (5 seconds)
+			setCountdown(5);
+		} catch (err: any) {
+			e.preventDefault();
+			setMessage(err.message);
+			setIsError(true);
+		}
+	};
 
-      return () => clearTimeout(timer);
-    } else if (countdown === 0 && !isError && message) {
-      navigate("/login");
-    }
-  }, [countdown, isError, message, navigate]);
+	// Countdown effect
+	useEffect(() => {
+		if (countdown > 0) {
+			const timer = setTimeout(() => {
+				setCountdown((prev) => prev - 1);
+			}, 1000);
 
-  // Redirect to homepage, if logged in
-  useEffect(() => {
-    const token = localStorage.getItem("token");
+			return () => clearTimeout(timer);
+		} else if (countdown === 0 && !isError && message) {
+			navigate("/login");
+		}
+	}, [countdown, isError, message, navigate]);
 
-    if (token) {
-      navigate("/");
-      console.log(token);
-    }
-  }, []);
+	// Redirect to homepage, if logged in
+	useEffect(() => {
+		const token = localStorage.getItem("token");
 
-  return (
-    <div>
-      <h1 className="text-center">Register</h1>
+		if (token) {
+			navigate("/");
+			console.log(token);
+		}
+	}, []);
 
-      {message && (
-        <h4 className={isError ? "error" : "success"}>
-          {message}{" "}
-          {!isError && countdown > 0 && (
-            <span>Redirecting to login in {countdown}...</span>
-          )}
-        </h4>
-      )}
+	return (
+		<div>
+			<h1 className="text-center">Register</h1>
 
-      <form onSubmit={onSubmit} className="registration_container auth-form">
-        <div className="name_container">
-          <input
-            type="text"
-            value={firstname}
-            onInput={(e) => setFirstname((e.target as HTMLInputElement).value)}
-            placeholder="Firstname"
-            className="input"
-            required
-          />
-          <input
-            type="text"
-            value={lastname}
-            onInput={(e) => setLastname((e.target as HTMLInputElement).value)}
-            placeholder="Lastname"
-            className="input"
-            required
-          />
-        </div>
+			{message && (
+				<h4 className={isError ? "error" : "success"}>
+					{message}{" "}
+					{!isError && countdown > 0 && (
+						<span>Redirecting to login in {countdown}...</span>
+					)}
+				</h4>
+			)}
 
-        <input
-          className="input"
-          type="email"
-          name="email"
-          value={email}
-          onInput={(e) => setEmail((e.target as HTMLInputElement).value)}
-          placeholder="Email"
-          required
-        />
+			<form onSubmit={onSubmit} className="registration_container auth-form">
+				<div className="name_container">
+					<input
+						type="text"
+						value={firstname}
+						onInput={(e) => setFirstname((e.target as HTMLInputElement).value)}
+						placeholder="Firstname"
+						className="input"
+						required
+					/>
+					<input
+						type="text"
+						value={lastname}
+						onInput={(e) => setLastname((e.target as HTMLInputElement).value)}
+						placeholder="Lastname"
+						className="input"
+						required
+					/>
+				</div>
 
-        <div className="password_container">
-          <input
-            className="password_input"
-            name="passwordInput"
-            type={showPassword ? "text" : "password"}
-            value={password}
-            onInput={(e) => setPassword((e.target as HTMLInputElement).value)}
-            placeholder="Password"
-            required
-          />
-          <button
-            type="button"
-            onClick={togglePasswordVisibility}
-            className="password_visibility"
-          >
-            {showPassword ? (
-              <FaEye color="#e0e0e0" size={16} />
-            ) : (
-              <FaEyeSlash color="#e0e0e0" size={16} />
-            )}
-          </button>
-        </div>
+				<input
+					className="input"
+					type="email"
+					name="email"
+					value={email}
+					onInput={(e) => setEmail((e.target as HTMLInputElement).value)}
+					placeholder="Email"
+					required
+				/>
 
-        <div className="password_container">
-          <input
-            className="password_input"
-            name="passwordInput"
-            type={showConfirmPassword ? "text" : "password"}
-            value={confirmPassword}
-            onInput={(e) =>
-              setConfirmPassword((e.target as HTMLInputElement).value)
-            }
-            placeholder="Confirm Password"
-            required
-          />
-          <button
-            type="button"
-            onClick={toggleConfirmPasswordVisibility}
-            className="password_visibility"
-          >
-            {showConfirmPassword ? (
-              <FaEye color="#e0e0e0" size={16} />
-            ) : (
-              <FaEyeSlash color="#e0e0e0" size={16} />
-            )}
-          </button>
-        </div>
+				<div className="password_container">
+					<input
+						className="password_input"
+						name="passwordInput"
+						type={showPassword ? "text" : "password"}
+						value={password}
+						onInput={(e) => setPassword((e.target as HTMLInputElement).value)}
+						placeholder="Password"
+						required
+					/>
+					<button
+						type="button"
+						onClick={togglePasswordVisibility}
+						className="password_visibility"
+					>
+						{showPassword ? (
+							<FaEye color="#e0e0e0" size={16} />
+						) : (
+							<FaEyeSlash color="#e0e0e0" size={16} />
+						)}
+					</button>
+				</div>
 
-        <Link to="/login" className="auth-redirect">
-          <h4>Have an account?</h4>
-        </Link>
+				<div className="password_container">
+					<input
+						className="password_input"
+						name="passwordInput"
+						type={showConfirmPassword ? "text" : "password"}
+						value={confirmPassword}
+						onInput={(e) =>
+							setConfirmPassword((e.target as HTMLInputElement).value)
+						}
+						placeholder="Confirm Password"
+						required
+					/>
+					<button
+						type="button"
+						onClick={toggleConfirmPasswordVisibility}
+						className="password_visibility"
+					>
+						{showConfirmPassword ? (
+							<FaEye color="#e0e0e0" size={16} />
+						) : (
+							<FaEyeSlash color="#e0e0e0" size={16} />
+						)}
+					</button>
+				</div>
 
-        <button type="submit">Register</button>
-      </form>
-    </div>
-  );
+				<Link to="/login" className="auth-redirect">
+					<h4>Have an account?</h4>
+				</Link>
+
+				<button type="submit">Register</button>
+			</form>
+		</div>
+	);
 };
 
 export default Register;
--- frontend/src/utils/auth.ts
+++ frontend/src/utils/auth.ts
@@ -4,63 +4,90 @@ import api from "./axios";
 /**
  * Login Request
  * --------------------
- * @param email 
- * @param password 
+ * @param email
+ * @param password
  * @returns {number} HTTP status code (e.g., 200 on success)
- * 
+ *
  * - On Success, Stores the JWT token to local storage.
  * - On Error, Returns the status code and should handle by the caller.
  */
 
 export async function login(email: string, password: string) {
-    try {
-        const res = await api.post("/auth/login", { email, password });
+	try {
+		const res = await api.post("/auth/login", { email, password });
 
-        const data = res.data;
-        localStorage.setItem("token", data.token);
+		const data = res.data;
+		localStorage.setItem("token", data.token);
 
-        return data;
-    } catch (err: any) {
-        if (err.response && (err.response.status === 401 || err.response.status === 403)) {
-            throw new Error(err.response.data.message || "Invalid email or password.");
-        } else {
-            throw new Error("Something went wrong. Please try again.");
-        }
-    }
+		return data;
+	} catch (err: any) {
+		console.log(err);
+		if (
+			err.response &&
+			(err.response.status === 401 || err.response.status === 403)
+		) {
+			throw new Error(
+				err.response.data.message || "Invalid email or password."
+			);
+		} else {
+			throw new Error("Something went wrong. Please try again.");
+		}
+	}
 }
 
-export async function register(firstname: string, lastname: string, email: string, password: string, confirmPassword: string) {
-    try {
-        const res = await api.post("/auth/register", { firstname, lastname, email, password, confirmPassword });
+export async function register(
+	firstname: string,
+	lastname: string,
+	email: string,
+	password: string,
+	confirmPassword: string
+) {
+	try {
+		const res = await api.post("/auth/register", {
+			firstname,
+			lastname,
+			email,
+			password,
+			confirmPassword,
+		});
 
-        return res;
-    } catch (err: any) {
-        const objects = err.response.data.messages;
-        const firstMessage = Object.values(objects)[0];
-        throw new Error(typeof firstMessage === "string" ? firstMessage : "Something went wrong.Please try again.");
-    }
+		return res;
+	} catch (err: any) {
+		const objects = err.response.data.messages;
+		const firstMessage = Object.values(objects)[0];
+		throw new Error(
+			typeof firstMessage === "string"
+				? firstMessage
+				: "Something went wrong.Please try again."
+		);
+	}
 }
 
 export const logout = async () => {
-    try {
-        const res = await api.post("/auth/logout");
-        localStorage.removeItem("token");
+	try {
+		const res = await api.post("/auth/logout");
+		localStorage.removeItem("token");
 
-        return res;
-    } catch (err: any) {
-        throw new Error(err.response.data.messages);
-    }
-}
+		return res;
+	} catch (err: any) {
+		throw new Error(err.response.data.messages);
+	}
+};
 
 export const verifyToken = async (setUserData: (data: any) => void) => {
-    try {
-        const res = await api.post("/auth/verify");
+	try {
+		const res = await api.post("/auth/verify");
 
-        setUserData({ name: res.data.name, email: res.data.email, role: res.data.role, isAuthenticated: true })
+		setUserData({
+			name: res.data.name,
+			email: res.data.email,
+			role: res.data.role,
+			isAuthenticated: true,
+		});
 
-        return res.status;
-    } catch (err: any) {
-        localStorage.removeItem("token");
-        throw new Error(err.response.data.message);
-    }
-}
+		return res.status;
+	} catch (err: any) {
+		localStorage.removeItem("token");
+		throw new Error(err.response.data.message);
+	}
+};
